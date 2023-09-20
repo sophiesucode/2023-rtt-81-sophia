d301094ef482635af5e8a9dd25349838a8749bf4
sophiesucode <57269207+sophiesucode@users.noreply.github.com>
2023-09-20 15:35:54 -0400

    variables example

---
+++ second-project/SecondProject/src/exercise/Variables.java
@@ -0,0 +1,18 @@
+package exercise;
+
+public class Variables {
+	public static void main(String[] args) {
+		String x = new String();
+		x.toLowerCase();
+		x.toUpperCase();
+		
+		int j = 4;
+		
+		Integer k = Integer.valueOf(5);
+
+		
+		boolean tuesday = true;
+		Boolean wednesday = new Boolean(false);
+	}
+
+}
